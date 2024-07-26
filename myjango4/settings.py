8ee55ed5388400f9eec39a97c5a7f9e894b696e8
fernando <s24hseguridad@gmail.com>
2024-07-26 17:33:59 -0500

    12

--- myjango4/settings.py
+++ myjango4/settings.py
@@ -55,9 +55,7 @@ ROOT_URLCONF = 'myjango4.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'myreact/build')
-        ],
+        'DIRS': [os.path.join(BASE_DIR, 'myreact/build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,15 +116,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR, 'myreact/build/static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'myreact/build/static'),
 ]
 
 
